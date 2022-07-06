00da333b59b899a45d7ad0031f096405a1685636
omermorad <omer.moradd@gmail.com>
2022-07-06 15:01:06 +0300

    chore: remove .idea

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/omer/projects/automock.dev/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/omer/projects/automock.dev/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/omer/projects/automock.dev/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/omer/projects/automock.dev/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/omer/projects/automock.dev/src/css/custom.css'),
-];
