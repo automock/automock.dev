67a2533003949d81b088cccd1024031ceacfc5e4
omermorad <omer.moradd@gmail.com>
2022-07-06 14:59:02 +0300

    chore: initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/Users/omer/projects/automock.dev/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/omer/projects/automock.dev/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/omer/projects/automock.dev/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/omer/projects/automock.dev/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/omer/projects/automock.dev/src/css/custom.css'),
+];
